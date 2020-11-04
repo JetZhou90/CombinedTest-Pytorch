5e20381da494cd52e31d99d44a849ba98634c547
JetZhou90 <18862392512@163.com>
2020-11-04 18:51:53 +0800

    hash

--- transformImg.py
+++ transformImg.py
@@ -2,6 +2,25 @@ import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
+from itertools import chain
+from scipy.signal import convolve2d
+ 
+def matlab_style_gauss2D(shape=(3,3),sigma=0.5):
+    """
+    2D gaussian mask - should give the same result as MATLAB's
+    fspecial('gaussian',[shape],[sigma])
+    """
+    m,n = [(ss-1.)/2. for ss in shape]
+    y,x = np.ogrid[-m:m+1,-n:n+1]
+    h = np.exp( -(x*x + y*y) / (2.*sigma*sigma) )
+    h[ h < np.finfo(h.dtype).eps*h.max() ] = 0
+    sumh = h.sum()
+    if sumh != 0:
+        h /= sumh
+    return h
+ 
+def filter2(x, kernel, mode='same'):
+    return convolve2d(x, np.rot90(kernel, 2), mode=mode)
 
 def image_show(img, axis='off',title=None,save=None):
     plt.figure("Image") # 图像窗口名称
@@ -81,10 +100,77 @@ def padding(image, target_size):
     new_img = cv2.copyMakeBorder(image, h_val,t_h - h-h_val,w_val,t_w - w-w_val,cv2.BORDER_REPLICATE)
     return new_img
 
+class PHash(object):
+    @staticmethod
+    def pHash(image):
+        """
+        get image pHash value
+        """
+        # 加载并调整图片为32x32灰度图片
+        img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        img = cv2.resize(img, (64, 64), interpolation=cv2.INTER_CUBIC)
+ 
+        # 创建二维列表
+        h, w = img.shape[:2]
+        vis0 = np.zeros((h, w), np.float32)
+        vis0[:h, :w] = img  # 填充数据
+ 
+        # 二维Dct变换
+        vis1 = cv2.dct(cv2.dct(vis0))
+        # cv.SaveImage('a.jpg',cv.fromarray(vis0)) #保存图片
+        vis1.resize((32, 32), refcheck=False)
+ 
+        # 把二维list变成一维list
+        img_list = list(chain.from_iterable(vis1))
+ 
+        # 计算均值
+        avg = sum(img_list) * 1. / len(img_list)
+        avg_list = ['0' if i < avg else '1' for i in img_list]
+    
+        # 得到哈希值
+        return ''.join(['%x' % int(''.join(avg_list[x:x + 4]), 2) for x in range(0, 32*32, 4)])
+ 
+    @staticmethod
+    def hammingDist(s1, s2):
+        """
+        计算两张图片的汉明距离
+        """
+        assert len(s1) == len(s2)
+        return sum([ch1 != ch2 for ch1, ch2 in zip(s1, s2)])
+
+
+def ssim(im1, im2, k1=0.01, k2=0.03, win_size=11, L=255):
+    if not im1.shape == im2.shape:
+        raise ValueError("Input Imagees must have the same dimensions")
+    if len(im1.shape) > 2:
+        im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+        im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+        # raise ValueError("Please input the images with 1 channel")
+
+    M, N = im1.shape
+    C1 = (k1*L)**2
+    C2 = (k2*L)**2
+    window = matlab_style_gauss2D(shape=(win_size,win_size), sigma=1.5)
+    window = window/np.sum(np.sum(window))
+    if im1.dtype == np.uint8:
+        im1 = np.double(im1)
+    if im2.dtype == np.uint8:
+        im2 = np.double(im2)
+    mu1 = filter2(im1, window, 'valid')
+    mu2 = filter2(im2, window, 'valid')
+    mu1_sq = mu1 * mu1
+    mu2_sq = mu2 * mu2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = filter2(im1*im1, window, 'valid') - mu1_sq
+    sigma2_sq = filter2(im2*im2, window, 'valid') - mu2_sq
+    sigmal2 = filter2(im1*im2, window, 'valid') - mu1_mu2
+    ssim_map = ((2*mu1_mu2+C1) * (2*sigmal2+C2)) / ((mu1_sq+mu2_sq+C1) * (sigma1_sq+sigma2_sq+C2))
+    return np.mean(np.mean(ssim_map))
+
 if __name__ == "__main__":
-    img_path = 'data/unet/train/images/new_10-11_801.png'
-    mask_path = 'data/unet/train/annotations/new_10-11_801.jpg'
-    condition_path = '../cmb_seal/rhombus.png'
+    img_path = 'data/unet/train/new_58-16_2850.png'
+    mask_path = 'data/unet/train/new_58-16_2850.jpg'
+    condition_path = '../cmb_seal/square_6.png'
     seal_condition = Image.open(condition_path)
     seal_condition = np.array(seal_condition)
     seal_condition = seal_condition[...,:3]
@@ -96,11 +182,18 @@ if __name__ == "__main__":
     resized_seal_img = padding(seal_img,(val,val))
     seal_condition = padding(seal_condition,(val,val))
     # resized_seal_img = cv2.resize(seal_image,(w,h))
-    trans_img = Transfrom_img(seal_condition,resized_seal_img,min_mathch_count=3)
+    trans_img = transfrom_img(seal_condition,resized_seal_img,min_mathch_count=3)
     # resized_seal_img = cv2.resize(trans_img,(w,h))
     if trans_img is None:
         image_show(np.hstack([resized_seal_img,seal_condition]))
         print('Not Match')
     else:
         image_show(np.hstack([resized_seal_img,trans_img,seal_condition]))
-        print(np.corrcoef(trans_img.flatten(),seal_condition.flatten())[0][1])
+        print('ssim:s',ssim(trans_img,seal_condition))
+        hash1 = PHash.pHash(trans_img)
+        hash2 = PHash.pHash(seal_condition)
+        distance = PHash.hammingDist(hash1, hash2)
+        print(distance)
+        out_score = 1 - distance * 1. / (32 * 32 / 4)
+        print('pHash Score:',out_score)
+        print('Corrcoef:', np.corrcoef(trans_img.flatten(),seal_condition.flatten())[0][1])
